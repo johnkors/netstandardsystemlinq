f9986becfceabbc90bd090d4c312b88420180096
John10 Korsnes <johnkors@gmail.com>
2016-11-26 10:45:35 +0100

    4.0.0

--- src/TestLib/Class1.cs
+++ src/TestLib/Class1.cs
@@ -1,9 +1,13 @@
-﻿namespace TestLib
+﻿using System.Linq;
+
+namespace TestLib
 {
     public class Class1
     {
         public Class1()
         {
+            var str = "hallo this 1 should be filterable";
+            var someOthBool = str.Any(c => c == '1');
         }
     }
 }
